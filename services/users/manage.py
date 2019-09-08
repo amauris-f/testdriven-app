56e417759d894125b0c1bdaad24794fae72d7169
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-09-08 18:59:58 -0400

    add libraries for code coverage/quality

--- services/users/manage.py
+++ services/users/manage.py
@@ -6,6 +6,17 @@ import sys
 from project import create_app, db
 from project.api.models import User
 
+import coverage
+COV = coverage.coverage(
+  branch=True,
+  include='project/*',
+  omit=[
+    'project/tests/*',
+    'project/config.py'
+  ]
+)
+COV.start()
+
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
@@ -31,6 +42,21 @@ def seed_db():
   db.session.add(User(username='michael', email='hermanmu@gmail.com'))
   db.session.add(User(username='michaelherman', email='michael@mherman.org'))
   db.session.commit()
+
+@cli.command()
+def cov():
+  """Runs the unit tests with coverage."""
+  tests = unittest.TestLoader().discover('project/tests')
+  result = unittest.TextTestRunner(verbosity=2).run(tests)
+  if result.wasSuccessful():
+    COV.stop()
+    COV.save()
+    print('Coverage summary')
+    COV.report()
+    COV.html_report()
+    COV.erase()
+    return 0
+  sys.exit(result)
 
 if __name__ == '__main__':
   cli()
