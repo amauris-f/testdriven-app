6b3822c38439bc2ded3cad3872696c8ed8bf5c16
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-09-08 19:36:37 -0400

    .py files formatted to follow pep-8 guidelines

--- services/users/manage.py
+++ services/users/manage.py
@@ -8,12 +8,12 @@ from project.api.models import User
 
 import coverage
 COV = coverage.coverage(
-  branch=True,
-  include='project/*',
-  omit=[
-    'project/tests/*',
-    'project/config.py'
-  ]
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py'
+    ]
 )
 COV.start()
 
@@ -23,40 +23,44 @@ cli = FlaskGroup(create_app=create_app)
 
 @cli.command('recreate_db')
 def recreate_db():
-  db.drop_all()
-  db.create_all()
-  db.session.commit()
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 @cli.command()
 def test():
-  """Runs the tests without code coverage"""
-  tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
-  result = unittest.TextTestRunner(verbosity=2).run(tests)
-  if result.wasSuccessful():
-    return 0
-  sys.exit(result)
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
 
 @cli.command('seed_db')
 def seed_db():
-  """Seeds the database."""
-  db.session.add(User(username='michael', email='hermanmu@gmail.com'))
-  db.session.add(User(username='michaelherman', email='michael@mherman.org'))
-  db.session.commit()
+    """Seeds the database."""
+    db.session.add(User(username='michael', email='hermanmu@gmail.com'))
+    db.session.add(User(username='michaelherman', email='michael@mherman.org'))
+    db.session.commit()
+
 
 @cli.command()
 def cov():
-  """Runs the unit tests with coverage."""
-  tests = unittest.TestLoader().discover('project/tests')
-  result = unittest.TextTestRunner(verbosity=2).run(tests)
-  if result.wasSuccessful():
-    COV.stop()
-    COV.save()
-    print('Coverage summary')
-    COV.report()
-    COV.html_report()
-    COV.erase()
-    return 0
-  sys.exit(result)
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage summary')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
+
 
 if __name__ == '__main__':
-  cli()
+    cli()
